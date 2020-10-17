65b1e760fdd6d606b9738edce385366789960420
Abishek Chozhan <abishekchozhan@berkeley.edu>
2020-10-17 16:18:05 -0700

    worked on more design, fixed location issue, intent now implemented from onboarding to main

--- represent/app/src/main/java/com/projectone/represent/MainActivity.java
+++ represent/app/src/main/java/com/projectone/represent/MainActivity.java
@@ -28,87 +28,104 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Make app fullscreen (i.e. no status bar)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        /* -- BEGIN SETUP -- */
         final EditText address_text = (EditText) findViewById(R.id.address_edit_text);
         final Button refresh_button = (Button) findViewById(R.id.refresh_button);
+
         /*final TextView first_name_view =
         final TextView second_name_view = (TextView) findViewById(R.id.name_second_view);
         final TextView third_name_view = (TextView) findViewById(R.id.name_third_view);*/
         view_list.add((TextView) findViewById(R.id.name_first_view));
         view_list.add((TextView) findViewById(R.id.name_second_view));
         view_list.add((TextView) findViewById(R.id.name_third_view));
-        final RequestQueue queue = Volley.newRequestQueue(this);
+        /* -- END SETUP -- */
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String formatted_address = extras.getString("FORMATTED_ADDRESS");
+            Log.e("PASSED", formatted_address);
+            reloadOfficials(formatted_address);
+            //String value = extras.getString("key");
+            //The key argument here must match that used in the other activity
+        }
 
 
         refresh_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                final ArrayList<Office> offices_list = new ArrayList<>();
-                final ArrayList<Official> officials_list = new ArrayList<>();
-
-                String replaced = address_text.getText().toString();
-                replaced = replaced.replaceAll(" ", "");
-                String url = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCa5SOrP4lGuotvRWYW9GPD_ZSn9lYQd-A&address=" + replaced;
-
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Log.e("REQ", "SUCCESS");
-                                //textView.setText("Response is: "+ response.substring(0,500));
-                                try {
-                                    JSONObject received_object = new JSONObject(response);
-                                    JSONArray received_offices = received_object.getJSONArray("offices");
-                                    for (int i = 0; i < received_offices.length(); i++) {
-                                        JSONObject received_office_obj = (JSONObject) received_offices.get(i);
-                                        String position = received_office_obj.getString("name");
-                                        if (position.equals("U.S. Senator") || position.equals("U.S. Representative")) {
-                                            Office received_office = new Office(received_office_obj);
-                                             offices_list.add(received_office);
-                                        }
-                                    }
-
-                                    Office office_one = offices_list.get(0);
-                                    //textView.setText(String.valueOf(office_one.getName()));
-
-                                    //TODO: Finish setting up Officials
-                                    JSONArray received_officials = received_object.getJSONArray("officials");
-                                    for (int i = 0; i < received_officials.length(); i++) {
-                                        JSONObject received_official_obj = (JSONObject) received_officials.get(i);
-                                        Official received_official = new Official(received_official_obj);
-                                        officials_list.add(received_official);
-                                    }
-
-                                    int count = 0;
-                                    for (int i = 0; i < offices_list.size(); i++) {
-                                        for (int j = 0; j < offices_list.get(i).getIndices().length; j++) {
-                                            int pos = offices_list.get(i).getIndices()[j];
-                                            view_list.get(count).setText(officials_list.get(pos).getName());
-                                            count++;
-                                        }
-                                    }
-                                    Official official_one = officials_list.get(office_one.getIndices()[0]);
-                                    //textView.setText(official_one.getParty());
-
-                                } catch (JSONException e) {
-                                    Log.e("REQ", e.getMessage());
-                                    e.printStackTrace();
-                                }
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //Log.e("REQ", error.getLocalizedMessage());
-                        //textView.setText("That didn't work!");
-                    }
-                });
-
-                queue.add(stringRequest);
+                reloadOfficials(address_text.getText().toString());
             }
         });
+
+
+    }
+
+    private void reloadOfficials(String address) {
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final ArrayList<Office> offices_list = new ArrayList<>();
+        final ArrayList<Official> officials_list = new ArrayList<>();
+
+        String replaced = address.replaceAll(" ", "");
+        String url = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyCa5SOrP4lGuotvRWYW9GPD_ZSn9lYQd-A&address=" + replaced;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("REQ", "SUCCESS");
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                        try {
+                            JSONObject received_object = new JSONObject(response);
+                            JSONArray received_offices = received_object.getJSONArray("offices");
+                            for (int i = 0; i < received_offices.length(); i++) {
+                                JSONObject received_office_obj = (JSONObject) received_offices.get(i);
+                                String position = received_office_obj.getString("name");
+                                if (position.equals("U.S. Senator") || position.equals("U.S. Representative")) {
+                                    Office received_office = new Office(received_office_obj);
+                                    offices_list.add(received_office);
+                                }
+                            }
+
+                            Office office_one = offices_list.get(0);
+                            //textView.setText(String.valueOf(office_one.getName()));
+
+                            //TODO: Finish setting up Officials
+                            JSONArray received_officials = received_object.getJSONArray("officials");
+                            for (int i = 0; i < received_officials.length(); i++) {
+                                JSONObject received_official_obj = (JSONObject) received_officials.get(i);
+                                Official received_official = new Official(received_official_obj);
+                                officials_list.add(received_official);
+                            }
+
+                            int count = 0;
+                            for (int i = 0; i < offices_list.size(); i++) {
+                                for (int j = 0; j < offices_list.get(i).getIndices().length; j++) {
+                                    int pos = offices_list.get(i).getIndices()[j];
+                                    view_list.get(count).setText(officials_list.get(pos).getName());
+                                    count++;
+                                }
+                            }
+                            Official official_one = officials_list.get(office_one.getIndices()[0]);
+                            //textView.setText(official_one.getParty());
+
+                        } catch (JSONException e) {
+                            Log.e("REQ", e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Log.e("REQ", error.getLocalizedMessage());
+                //textView.setText("That didn't work!");
+            }
+        });
+
+        queue.add(stringRequest);
     }
 }
--- represent/app/src/main/java/com/projectone/represent/OnboardingActivity.java
+++ represent/app/src/main/java/com/projectone/represent/OnboardingActivity.java
@@ -1,14 +1,152 @@
 package com.projectone.represent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
 
 public class OnboardingActivity extends AppCompatActivity {
-
+    private FusedLocationProviderClient fusedLocationClient;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private String passedAddress = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        Button location_button = (Button) findViewById(R.id.location_button);
+        Button intent_button = (Button) findViewById(R.id.intent_button);
+
+
+        location_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkLocationPermission() != false) {
+                    getLocation();
+                }
+            }
+        });
+
+        intent_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                intent.putExtra("FORMATTED_ADDRESS", passedAddress);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void updateAddress(double latitude, double longitude) {
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final EditText location_text_view = (EditText) findViewById(R.id.location_text_view);
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + String.valueOf(latitude) + "," + String.valueOf(longitude) + "&key=AIzaSyCa5SOrP4lGuotvRWYW9GPD_ZSn9lYQd-A";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("REQ", "SUCCESS");
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                        try {
+                            JSONObject received_object = new JSONObject(response);
+                            JSONArray received_results = received_object.getJSONArray("results");
+                            JSONObject received_address = (JSONObject) received_results.get(0);
+                            String formatted_address = received_address.getString("formatted_address");
+                            Log.e("LOC", formatted_address);
+                            location_text_view.setText(formatted_address);
+                            passedAddress = formatted_address;
+                        } catch (JSONException e) {
+                            Log.e("LOC_E", e.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Log.e("REQ", error.getLocalizedMessage());
+                //textView.setText("That didn't work!");
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void getLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    // Logic to handle location object
+                    Log.e("LOC", String.valueOf(location.getLatitude()));
+                    updateAddress(location.getLatitude(), location.getLongitude());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        getLocation();
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
     }
 }
